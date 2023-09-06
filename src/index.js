cb49eef71af7f36968253e261ea49bbe91b1dd68
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-06 23:10:44 +0700

    created __dirname again

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@
 
 import Fastify from "fastify";
 import fastifyCors from "@fastify/cors";
+import fsPromises from "node:fs/promises";
+import url from "node:url";
+import path from "node:path";
 import { env } from "./lib/env.js";
 
 const server = Fastify();
+
+const fileName = url.fileURLToPath(import.meta.url);
+const directoryName = path.dirname(fileName);
 
 async function main() {
     try {
