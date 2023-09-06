4ebff8c048e20c325608bd2ec275f6013c66f0d6
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-06 22:45:54 +0700

    imported necessary things in index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// @ts-check
+
+import Fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import { env } from "./lib/env.js";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
