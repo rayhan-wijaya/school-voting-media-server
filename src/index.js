386d6fad2a6b2376fb84f00131a5b4be66662ff5
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-06 22:46:28 +0700

    created main() entry point function

--- src/index.js
+++ src/index.js
@@ -10,3 +10,19 @@ const server = Fastify();
 
 const fileName = fileURLToPath(import.meta.url);
 const directoryName = path.dirname(fileName);
+
+async function main() {
+    try {
+        server.register(fastifyStatic, {
+            root: path.join(directoryName, "uploads"),
+            prefix: "/image/",
+        });
+
+        server.listen({ host: env.HOST, port: env.PORT });
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+main();
