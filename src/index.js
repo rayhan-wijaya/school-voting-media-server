322076a22467514f74a7498bf9d7f0caf98d554c
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-06 22:46:10 +0700

    server and __dirname fill-in

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ import fastifyStatic from "@fastify/static";
 import { env } from "./lib/env.js";
 import { fileURLToPath } from "node:url";
 import path from "node:path";
+
+const server = Fastify();
+
+const fileName = fileURLToPath(import.meta.url);
+const directoryName = path.dirname(fileName);
