22dff170b20991268474bd220d26939e4f5aeb73
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-06 23:07:10 +0700

    keepin things simple

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 // @ts-check
 
 import Fastify from "fastify";
-import fastifyStatic from "@fastify/static";
-import url from "node:url";
-import path from "node:path";
+import fastifyCors from "@fastify/cors";
 import { env } from "./lib/env.js";
 
 const server = Fastify();
 
-const fileName = url.fileURLToPath(import.meta.url);
-const directoryName = path.dirname(fileName);
-
 async function main() {
     try {
-        server.register(fastifyStatic, {
-            root: path.join(directoryName, "uploads"),
-            prefix: "/image/",
+        server.register(fastifyCors, {
+            origin: true,
         });
 
         server.listen({
